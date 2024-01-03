4e601826e552203e6c0907cf603ef1d50d5abda8
Benjamin Torfu <btorfu@proton.me>
2024-01-03 05:30:38 +0000

    cq

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in range(ord('a', ord('z') + 1)):
-    if (i != e & i != q):
+for i in range(ord('a'), ord('z') + 1):
+    if (i != 'e' and i != 'q'):
         print(chr(i), end=' ')
