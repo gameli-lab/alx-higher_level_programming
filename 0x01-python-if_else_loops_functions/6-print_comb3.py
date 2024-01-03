15a4a9917435fd60f7cf8998aef30648cf060dbd
Benjamin Torfu <btorfu@proton.me>
2024-01-03 07:59:33 +0000

    num

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for i in range(100):
+    for j in range(i +1, 10):
+        if (i != j):
+            if (i == 98):
+                print("{:02}".format(i * 10 + j),end='')
+            else:
+                print("{:02}".format(i * 10 + j),end=', ')
