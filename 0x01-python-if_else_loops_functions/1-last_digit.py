b5ade055ab7b2c97558cee00653357a09a3f2774
Benjamin Torfu <btorfu@proton.me>
2024-01-03 04:40:47 +0000

    pacth

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,9 +6,9 @@ if (number < 0):
     last *= -1
 if (last > 5):
     print("Last digit of {} is {} and is greater
-          \than {}".format(number, last, "5"))
+          than {}".format(number, last, "5"))
 elif (last == 0):
     print("Last digit of {} is {} and is {}".format(number, last, last))
 else:
     print("Last digit of {} is {} and is less 
-            \than {} and not {}".format(number, last, "6", "0"))
+            than {} and not {}".format(number, last, "6", "0"))
