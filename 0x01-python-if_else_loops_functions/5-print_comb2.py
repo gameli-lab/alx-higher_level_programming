bae95a528d99eea73b853a268278455e0cbc43e5
Benjamin Torfu <btorfu@proton.me>
2024-01-03 06:35:31 +0000

    nums

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
-    print("{}".format(i), end=',')
+    if (i == 99):
+        print("{}".format(str(i).zfill(2)), end='\n')
+    else:
+        print("{}".format(str(i).zfill(2)), end=', ')
