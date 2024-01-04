5a010bdfe27d48fc653464760d03564aace3f499
Benjamin Torfu <btorfu@proton.me>
2024-01-04 20:32:19 +0000

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
+    def main():
     import sys
     num_args = len(sys.argv) - 1
     if num_args == 0:
@@ -10,3 +10,6 @@ if __name__ == "__main__":
         print("{} arguments:".format(num_args))
         for i, arg in enumerate(sys.argv[1:], 1):
             print("{}: {}".format(i, arg))
+
+
+if __name__ == "__main__":                            main():
