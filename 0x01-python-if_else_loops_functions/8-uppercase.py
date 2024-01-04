d3f9ce9827de28d8e51fa94f880f2b00b2e0835e
Benjamin Torfu <btorfu@proton.me>
2024-01-04 04:37:41 +0000

    uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,8 +2,9 @@
 def uppercase(str):
     result = ""
     for char in str:
-    if ('a' <= str <= 'z'):
-        result += chr(ord(char)) - ord('a') + ord('A')
-    else:
-        result += char
+        if ('a' <= str <= 'z'):
+            result += chr(ord(char)) - ord('a') + ord('A')
+        else:
+            result += char
+
     return (result)
