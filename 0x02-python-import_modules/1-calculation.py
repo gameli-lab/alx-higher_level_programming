e7b063685aa13036ff0ea384f358e9d5024f640d
Benjamin Torfu <btorfu@proton.me>
2024-01-04 14:56:29 +0000

    calc

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    a = 10
+    b = 5
+
+    p = a + b
+    q = a - b
+    r = a * b
+    s = a / b
+
+    print("{} + {} = {}".format(a, b, p))
+    print("{} - {} = {}".format(a, b, q))
+    print("{} * {} = {}".format(a, b, r))
+    print("{} / {} = {}".format(a, b, s))
