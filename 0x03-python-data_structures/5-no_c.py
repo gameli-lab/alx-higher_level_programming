2a3a2ccd890dad3898faad5680ee0bd11d9d0046
Benjamin Torfu <btorfu@proton.me>
2024-01-08 09:44:49 +0000

    no c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -6,7 +6,3 @@ def no_c(my_string):
             new_string += item
 
     return (new_string)
-
-"""print("{}".format(new_string))
-new_string = ''.join(item for item in my_string if item.lower() not in ['c', 'C'])
-    print("{}".format(new_string))"""
