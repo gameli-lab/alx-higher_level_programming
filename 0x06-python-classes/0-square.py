d549bd4a58c6be6aa5dfe21acf05af97b7d23223
Benjamin Torfu <btorfu@proton.me>
2024-01-23 23:38:57 +0000

    docstr

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -3,9 +3,7 @@
 # -*- coding: utf-8 -*-
 
 class Square:
-    """
-    This class defines empty squares.
-    It is an empty class definition for now
-
+    """This class defines empty squares.
+        It is an empty class definition for now
     """
     pass
