38a2cf26c18d6aa81128a6651376d3486ac0731e
Benjamin Torfu <btorfu@proton.me>
2024-01-24 05:37:57 +0000

    again

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
 class Square:
-    """This class defines empty squares.
-        It is an empty class definition for now."""
+    """This class defines empty squares."""
     pass
