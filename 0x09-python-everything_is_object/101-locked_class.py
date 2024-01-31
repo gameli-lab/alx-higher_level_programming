28b6aaaaf205dd33711f80592e580ccbc2eefca0
Benjamin Torfu <btorfu@proton.me>
2024-01-31 05:20:19 +0000

    class

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''This module defines a locked class'''
+
+
+class LockedClass:
+    '''defines a locked class'''
+    __slots__ = ('first_name', )
+
+    def __init__(self, first_name):
+        '''initiates the class'''
+        self.first_name = firs_tname
