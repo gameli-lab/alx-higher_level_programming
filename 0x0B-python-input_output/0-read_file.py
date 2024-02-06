9038536bab8d6d5005e23a275cb0eb8cb6b093de
Benjamin Torfu <btorfu@proton.me>
2024-02-06 21:22:43 +0000

    read

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,6 +6,5 @@
 def read_file(filename=""):
     """ This function reads a file"""
     with open(filename, encoding="utf-8") as file:
-        for line in file:
-            print(line)
+        print(file.read())
 
