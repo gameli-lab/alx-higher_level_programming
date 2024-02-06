fda3971528ff6c944dbd44cf1b6b009a4408711b
Benjamin Torfu <btorfu@proton.me>
2024-02-06 23:24:44 +0000

    json

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -2,6 +2,7 @@
 """This module returns the JSON representation of an object (string)"""
 import json
 
+
 def to_json_string(my_obj):
     """This finction returns json rep of obj"""
-    print(json.dumps(my_obj))
+    return (json.dumps(my_obj))
