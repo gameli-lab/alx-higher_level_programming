80e9d073edb2d42b12678c7ccfad8c918e094551
Benjamin Torfu <btorfu@proton.me>
2024-02-06 23:49:07 +0000

    json

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -6,4 +6,4 @@ import json
 def save_to_json_file(my_obj, filename):
     """This class saves to json file"""
     with open(filename, "w", "utf-8") as file:
-        return (file.write(json.dumps(my_obj)))
+        json.dump(my_obj, file)
