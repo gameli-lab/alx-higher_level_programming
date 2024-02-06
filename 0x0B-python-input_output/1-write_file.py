5523835fbf2ae82a09a49a52ac2633a1b06f4343
Benjamin Torfu <btorfu@proton.me>
2024-02-06 22:32:50 +0000

    apend

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,4 +5,4 @@
 def write_file(filename="", text=""):
     """ This function writes to a file"""
     with open(filename, mode='w', encoding='utf-8') as file:
-        file.write(text)
+        return (file.write(text))
