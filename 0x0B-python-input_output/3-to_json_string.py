35cbae1c5197c152f5aba9b19875d052bb5f236a
Benjamin Torfu <btorfu@proton.me>
2024-02-06 23:18:56 +0000

    json

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This module returns the JSON representation of an object (string)"""
+import json
+
+def to_json_string(my_obj):
+    """This finction returns json rep of obj"""
+    print(json.dumps(my_obj)
