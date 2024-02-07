1cf15a6b783c36a4f26ddbae993004fcdbd118b4
Benjamin Torfu <btorfu@proton.me>
2024-02-07 00:00:42 +0000

    json

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module creates an Object from a json file"""
+
+
+def load_from_json_file(filename):
+    """This function creates obj from json"""
+    with open(filename, encoding="utf-8") as file:
+        json.loads(file)
