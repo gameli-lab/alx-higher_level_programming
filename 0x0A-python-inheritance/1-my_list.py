3d4a07bd770a989c81388efeaca6f0973322dd4c
Benjamin Torfu <btorfu@proton.me>
2024-02-07 12:22:27 +0000

    lookup

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module inherits from list"""
+
+
+class MyList(list):
+    """This class has inherited list"""
+    def print_sorted(self):
+        """This function prints sorted list"""
+        return (sorted(self))
