fc4941afd97d4587acca3cecea6291be880629a6
Benjamin Torfu <btorfu@proton.me>
2024-02-07 00:37:20 +0000

    json

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,7 +2,7 @@
 """This module adds all arguments to a Python list, and then save them to a file"""
 import sys
 
-if "__name__"="__main__":
+if "__name__"=="__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
     load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
