cda4f6f00941a62b7d69ec75309d6d534a33176a
Benjamin Torfu <btorfu@proton.me>
2024-02-07 00:08:45 +0000

    json

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -6,4 +6,4 @@ import json
 def load_from_json_file(filename):
     """This function creates obj from json"""
     with open(filename) as file:
-       return (json.load(file))
+        return (json.load(file))
