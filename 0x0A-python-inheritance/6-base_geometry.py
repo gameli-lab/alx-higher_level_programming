528aef3ac1722301d5f1ef6fe9160416a8ded380
Benjamin Torfu <btorfu@proton.me>
2024-02-08 05:20:57 +0000

    area

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''this module defines a class base geometry'''
+
+
+class BaseGeometry:
+    '''this class is for base geometry'''
+    def area(self):
+        '''this message defines an area'''
+        raise Exception("area() is not implemented")
