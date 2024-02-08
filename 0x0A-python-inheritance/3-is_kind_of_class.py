2e7a92859e42486d6772cb8c34f2e292b0be145e
Benjamin Torfu <btorfu@proton.me>
2024-02-08 04:50:03 +0000

    instance

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+'''This module find similar instance'''
+
+
+def is_kind_of_class(obj, a_class):
+    '''This function finds an instance'''
+    return (isinstance(obj, a_class))
