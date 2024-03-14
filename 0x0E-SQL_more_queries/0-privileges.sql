a68ae2bb9f1a4dd4399d67fb336a89ab7386b239
Benjamin Torfu <btorfu@proton.me>
2024-03-14 01:59:54 -0400

    sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,5 @@
 --This script ists all privileges of the MySQL users 
 
-"SHOW GRANTS FOR 'user_0d_1'@'localhost';"
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
 
-"SHOW GRANTS FOR 'user_0d_2'@'localhost';"
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
