3eccc3a7ef6d8f9108fa5387616269e48949df4d
Benjamin Torfu <btorfu@proton.me>
2024-03-14 00:47:14 -0400

    sql

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,6 @@
+--This is a Write a script that creates the MySQL server user user_0d_1.
+
+CREATE USER IF NOT EXIST 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_pwd';
+
+--Grant all priveleges
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
