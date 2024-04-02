f35eb4a297ca17c140d171aba2c8cc8bbe252db9
Banjamin Torfu <btorfu@proton.me>
2024-04-02 16:48:01 +0000

    five

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,2 @@
 -- Describes a table in details
-SELECT column_name, data_type, character_maximum_length, is_nullable, column_default
-FROM information_schema.schema
-WHERE table_schema='hbtn_0c_0' AND table_name='first_table';
+SHOW CREATE TABLE hbtn_0c_0.first_table
