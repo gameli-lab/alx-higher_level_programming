1d596b1f70df26ab3f5b5fae9bb10decb9cebd07
Banjamin Torfu <btorfu@proton.me>
2024-04-04 15:01:26 +0000

    avg

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,13 @@
 -- Moving to utf8
+CREATE DATABASE IF NOT EXSISTS hbtn_0c_0;
+USE hbtn_0c_0;
+
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+
 
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
