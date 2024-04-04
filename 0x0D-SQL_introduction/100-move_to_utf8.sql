94925f57758af4f24bcead165a53131209b4cc17
Banjamin Torfu <btorfu@proton.me>
2024-04-04 13:41:57 +0000

    utf8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,7 @@
 -- Moving to utf8
+
+USE hbtb_0c_0;
+
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
