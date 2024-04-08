aaac8c0b198be01b2a4c4d6ba8db898f18c630f3
Banjamin Torfu <btorfu@proton.me>
2024-04-08 20:10:39 +0000

    js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -3,4 +3,4 @@
 const c = 'C is fun';
 const p = 'Python is cool';
 const j = 'Javascript is amazing'; */
-console.log('C is fun\nPython is cool\nJavascript is amazing');
+console.log('C is fun\nPython is cool\nJavaScript is amazing');
