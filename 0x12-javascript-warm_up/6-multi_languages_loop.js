e2ef073b03f6424336755bb50d54d0ded688236f
Banjamin Torfu <btorfu@proton.me>
2024-04-08 21:52:07 +0000

    loop

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,8 +2,7 @@
 
 const lang = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let i = 0;
-while (i < 3)
-{
-	console.log(lang[i]);
-	i++;
+while (i < 3) {
+  console.log(lang[i]);
+  i++;
 }
