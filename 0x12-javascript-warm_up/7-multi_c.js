0455f419561729b36c01e1a5128cbf6b6641538d
Banjamin Torfu <btorfu@proton.me>
2024-04-08 22:03:08 +0000

    c loop

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,6 +3,8 @@
 const lang = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let i = 0;
 let j = process.argv[2];
+if (isNaN(j))
+	console.log('Missing number of occurrences');
 while (i < j) {
   console.log(lang[0]);
   i++;
