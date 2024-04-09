f54df0b9aa0c45360dfb2d7d22d3f04c7f60f71b
Banjamin Torfu <btorfu@proton.me>
2024-04-09 20:48:45 +0000

    class

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+	  this.w = width;
+	  this.h = height;
+  }
+}
+
+module.exports = Rectangle;
---
+++ 0x13-javascript_objects_scopes_closures/README.md
@@ -0,0 +1,1 @@
+JavaScript project - classes and objects
