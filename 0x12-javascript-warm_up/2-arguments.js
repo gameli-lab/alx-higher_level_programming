7270730112722ae2aeda9219ac1ad12a45e627ee
Banjamin Torfu <btorfu@proton.me>
2024-04-09 04:48:58 +0000

    export

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,9 +2,4 @@
 
 const index = process.argv.length;
 
-if(index < 3)
-	console.log('No argument');
-else if(index === 3)
-	console.log('Argument found');
-else
-	console.log('Arguments found');
+if (index < 3) { console.log('No argument'); } else if (index === 3) { console.log('Argument found'); } else { console.log('Arguments found'); }
