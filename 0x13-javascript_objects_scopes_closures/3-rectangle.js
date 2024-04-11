0ddea1a0421e31c5b0fa472880c65cc79dc5a29e
Benjamin Torfu <btorfu@proton.me>
2024-04-11 13:07:37 +0000

    rec

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -2,12 +2,18 @@
 
 class Rectangle {
   constructor (w, h) {
-    if (w < 1 || h < 1) {
-      this.width = ;
-      this.height = ;
-    } else {
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
+    }
+  }
+
+  print () {
+    if (!this.width || !this.height) { return; }
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) { row += 'X'; }
+      console.log(row);
     }
   }
 }
