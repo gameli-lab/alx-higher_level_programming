674ccdd51f1138eae9305f3d2d6225acec6cc0cf
Benjamin Torfu <btorfu@proton.me>
2024-04-11 16:24:35 +0000

    log

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,7 +1,3 @@
 #!/usr/bin/node
-
-exports.logMe = function (item) {
-  if (!this.count) { this.count = 0; }
-  this.count++;
-  console.log(`${this.count}:${item}`);
-};
+let count = 0;
+exports.logMe = function (item) { console.log(`${count++}: ${item}`); };
