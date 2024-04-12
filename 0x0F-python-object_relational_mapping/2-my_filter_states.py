266af70fffb6ccab0a3d451262bb8894ad9179cd
Banjamin Torfu <btorfu@proton.me>
2024-04-12 16:08:32 +0000

    2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+''' This is mysqldb module'''
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+
+    '''
+    This module lists all states in asc order
+    '''
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    inp = argv[4]
+    cursor = db.cursor()
+
+    query = "SELECT * FROM states WHERE name='{}'".format(inp)
+    cursor.execute(query)
+
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
