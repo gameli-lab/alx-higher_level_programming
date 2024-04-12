df244710d8c19bf19a75702458d3b0f8d6085f9e
Benjamin Torfu <btorfu@proton.me>
2024-04-12 12:45:32 +0000

    db

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -11,11 +11,11 @@ def list_states(username, password, database_name):
     This module lists all states in asc order
     '''
     db = MySQLdb.connect(host="localhost", port=3306, user=username,
-                         passwd=password, db=databasename)
+                         passwd=password, db=database_name)
 
     cursor = db.cursor()
 
-    query = "SELECT * FROM ststes ORDER BY states.id ASC"
+    query = "SELECT * FROM states ORDER BY states.id ASC"
     cursor.execute(query)
 
     rows = cursor.fetchall()
@@ -23,4 +23,4 @@ def list_states(username, password, database_name):
         print(row)
 
     cursor.close()
-    db.close
+    db.close()
