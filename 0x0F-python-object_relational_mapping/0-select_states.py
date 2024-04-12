890df2fd8f1c8dfc5cfefc763dd78ef50cce6c66
Banjamin Torfu <btorfu@proton.me>
2024-04-12 15:34:01 +0000

    db

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+''' This is mysqldb module'''
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+
+    '''
+    This module lists all states in asc order
+    '''
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+
+    cursor = db.cursor()
+
+    query = "SELECT * FROM states ORDER BY states.id ASC"
+    cursor.execute(query)
+
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
