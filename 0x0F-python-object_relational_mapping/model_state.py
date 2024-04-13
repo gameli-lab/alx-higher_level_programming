73660f138b50bfb64bd59d75772246ddec12d3bf
Banjamin Torfu <btorfu@proton.me>
2024-04-13 09:36:55 +0000

    alchemy

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,7 +2,7 @@
 
 ''' This is mysqldb module'''
 
-from sqlalchemy import * '''Column, Integer, String, create_engine'''
+from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sys import argv
 
