5a2487d540eb44eaab24a5fdff357df9751876f8
Banjamin Torfu <btorfu@proton.me>
2024-04-14 14:20:44 +0000

    model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -7,8 +7,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sys import argv
 
 if __name__ == "__main__":
-    engine = create_engine('mysql://{}:{}@localhost:3306/{}'
-                           .format(argv[1], argv[2], argv[3]))
+'''    engine = create_engine('mysql://{}:{}@localhost:3306/{}'
+                           .format(argv[1], argv[2], argv[3]))'''
 
     Base = declarative_base()
 
@@ -22,4 +22,4 @@ if __name__ == "__main__":
                     nullable=False)
         name = Column(String(128), nullable=False)
 
-    Base.metadata.create_all(engine)
+   ''' Base.metadata.create_all(engine)'''
