cd03875359430872244d0691f0427dbef20c275e
Banjamin Torfu <btorfu@proton.me>
2024-04-14 16:17:02 +0000

    list state

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+''' This is mysqldb module'''
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sys import argv
+
+
+if __name__ == "__main__":
+
+    '''
+    This module lists all states in asc order
+    '''
+
+    engine = create_engine("mysql://{}:{}@localhost:3306/{}"
+                           .format(argv[1], argv[2], argv[3]))
+    Base.metadata.bind = engine
+    dbs = sessionmaker(bind=engine)
+    session = dbs()
+
+    states = session.query(State).order_by(State.id.asc()).all()
+
+    for i, state in enumerate(states, start=1):
+        if (i == 1):
+            print('{}: {}'.format(i, state.name))
+
+    session.close()
