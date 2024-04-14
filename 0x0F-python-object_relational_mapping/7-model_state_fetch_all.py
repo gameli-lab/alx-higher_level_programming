69ff5f5f0a3d718b0b7cdc024b7c08141e2c77ff
Banjamin Torfu <btorfu@proton.me>
2024-04-14 15:58:01 +0000

    list states

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -14,14 +14,15 @@ if __name__ == "__main__":
     This module lists all states in asc order
     '''
 
-    engine = create_engine("mysql://{}:{}@localhost:3306/{}".format(argv[1], argv[2], argv[3]))
+    engine = create_engine("mysql://{}:{}@localhost:3306/{}".format(argv[1],
+                                                                    argv[2], argv[3]))
     Base.metadata.bind = engine
     dbs = sessionmaker(bind=engine)
     session = dbs()
 
     states = session.query(State).order_by(State.id.asc()).all()
 
-    for i,state in enumerate(states, start=1):
+    for i, state in enumerate(states, start=1):
         print('{}: {}'.format(i, state.name))
 
     session.close()
