c641afc50a0abb30147b50d17caa46f1dc12ada3
Banjamin Torfu <btorfu@proton.me>
2024-04-14 13:45:08 +0000

    model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-''' This is mysqldb module
+''' This is mysqldb module'''
 
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
@@ -13,24 +13,13 @@ if __name__ == "__main__":
     Base = declarative_base()
 
     class States(Base):
+        '''
+        States inherits from Base
+        '''
         __tablename__ = 'states'
 
         id = Column(Integer, primary_key=True, autoincrement=True,
                     nullable=False)
         name = Column(String(128), nullable=False)
 
-    Base.metadata.create_all(engine)
-
-#!/usr/bin/python3
-"""Start link class to table in database
-"""'''
-from sys import argv
-from model_state import Base, State
-from sqlalchemy import (create_engine)
-
-if __name__ == "__main__":
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'
-        .format(argv[1], argv[2],
-                argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
