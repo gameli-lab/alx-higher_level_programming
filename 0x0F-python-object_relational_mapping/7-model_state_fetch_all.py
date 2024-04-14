faac58b6ce14b65dbce410845b3b8be67d0f2365
Banjamin Torfu <btorfu@proton.me>
2024-04-14 15:37:10 +0000

    list states

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+''' This is mysqldb module'''
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sys import argv
+
+
+if __name__ == "__main__":
+
+    '''
+    This module lists all states in asc order
+    '''
+
+    engine = create_engine("mysql://{}:{}@localhost:3306/{}".format(argv[1], argv[2], argv[3]))
+    Base.metadata.bind = engine
+    dbs = sessionmaker(bind=engine)
+    session = dbs()
+
+    states = session.query(State).order_by(State.id.asc()).all()
+
+    for state in states:
+        print(state.name)
+
+    session.close()
