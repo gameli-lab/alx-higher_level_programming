b2a59b49e7fe30cf762c29d48f930dbc15f06663
Banjamin Torfu <btorfu@proton.me>
2024-04-14 14:30:32 +0000

    model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -10,16 +10,17 @@ if __name__ == "__main__":
     engine = create_engine('mysql://{}:{}@localhost:3306/{}'
                            .format(argv[1], argv[2], argv[3]))'''
 
-    Base = declarative_base()
+Base = declarative_base()
 
-    class States(Base):
-        '''
-        States inherits from Base
-        '''
-        __tablename__ = 'states'
 
-        id = Column(Integer, primary_key=True, autoincrement=True,
-                    nullable=False)
-        name = Column(String(128), nullable=False)
+class States(Base):
+    '''
+    States inherits from Base
+    '''
+    __tablename__ = 'states'
 
-   ''' Base.metadata.create_all(engine)'''
+    id = Column(Integer, primary_key=True, autoincrement=True,
+                nullable=False)
+    name = Column(String(128), nullable=False)
+
+    ''' Base.metadata.create_all(engine)'''
