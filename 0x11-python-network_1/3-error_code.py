9388551a78e17a862a0ef9112f247c3c55066029
Benjamin Torfu <btorfu@proton.me>
2024-04-26 18:21:46 +0000

    error

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -3,10 +3,16 @@
 import urllib.request
 import sys
 
-url = sys.argv[1]
-try:
-    with urllib.request.urlopen(url) as res:
-        body = res.read().decode('utf-8')
-        print(body)
-except urllib.error.HTTPError as e:
-    print('Error code: {}'.format(e.code))
+def main(argv):
+    '''managing http error code'''
+    url = sys.argv[1]
+    req = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(req) as res:
+            body = res.read().decode('utf-8')
+            print(body)
+    except urllib.error.HTTPError as e:
+        print('Error code: {}'.format(e.code))
+
+if __name__ == '__main__':
+    main(argv)
