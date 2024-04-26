940b2b87324585e688ca21f161d48c67bbc5cf66
Benjamin Torfu <btorfu@proton.me>
2024-04-26 18:30:50 +0000

    error

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-'''header module
+'''header module'''
 import urllib.request
 import sys
 
@@ -15,22 +15,4 @@ def main(argv):
         print('Error code: {}'.format(e.code))
 
 if __name__ == '__main__':
-    main(argv)'''
-
-
-"""A script that:
-- takes in a URL,
-- sends a request to the URL
-- displays the body of the response (decoded in utf-8).
-"""
-
-
-if __name__ == "__main__":
-    import sys
-    from urllib import request, error
-
-    try:
-        with request.urlopen(sys.argv[1]) as res:
-            print(res.read().decode('UTF-8'))
-    except error.HTTPError as er:
-        print('Error code:', er.code)
+    main(argv)
