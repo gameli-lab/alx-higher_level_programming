cc61582e208f7480404fd3903e547913635409ee
Benjamin Torfu <btorfu@proton.me>
2024-04-26 17:13:51 +0000

    mail

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''header module'''
+import urllib.request
+import sys
+
+url = sys.argv[1]
+email = sys.argv[2]
+coded = email.encode('UTF-8')
+
+req = urllib.request.Request(url, data=coded, method='POST')
+with urllib.request.urlopen(req) as res:
+    body = res.read().decode('UTF-8')
+
+    print(body)
+
+    print('Your email is: {}'.format(body))
