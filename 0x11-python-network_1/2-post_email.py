474e2ed3a23b6876ef1054a824850e14f6447e37
Benjamin Torfu <btorfu@proton.me>
2024-04-26 17:18:52 +0000

    mail

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-'''header module'''
+'''header module
 import urllib.request
 import sys
 
@@ -11,6 +11,18 @@ req = urllib.request.Request(url, data=coded, method='POST')
 with urllib.request.urlopen(req) as res:
     body = res.read().decode('UTF-8')
 
-    print(body)
+    print(body)'''
 
-    print('Your email is: {}'.format(body))
+import urllib.request
+import sys
+
+url = sys.argv[1]
+email = sys.argv[2]
+
+data = email.encode('utf-8')
+
+req = urllib.request.Request(url, data=data, method='POST')
+
+with urllib.request.urlopen(req) as response:
+    body = response.read().decode('utf-8')
+    print(body)
