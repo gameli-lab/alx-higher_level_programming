44d98a52bd6a6568c89d54a4062d06983d01c740
Benjamin Torfu <btorfu@proton.me>
2024-04-26 19:00:09 +0000

    error

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,28 +1,14 @@
 #!/usr/bin/python3
-'''header module
+'''header module'''
 import urllib.request
 import sys
+if __name__ == '__main__':
+    url = sys.argv[1]
+    email = sys.argv[2]
+    coded = email.encode('UTF-8')
 
-url = sys.argv[1]
-email = sys.argv[2]
-coded = email.encode('UTF-8')
+    req = urllib.request.Request(url, data=coded, method='POST')
+    with urllib.request.urlopen(req) as res:
+        body = res.read().decode('UTF-8')
 
-req = urllib.request.Request(url, data=coded, method='POST')
-with urllib.request.urlopen(req) as res:
-    body = res.read().decode('UTF-8')
-
-    print(body)'''
-
-import urllib.request
-import sys
-
-url = sys.argv[1]
-email = sys.argv[2]
-
-data = email.encode('utf-8')
-
-req = urllib.request.Request(url, data=data, method='POST')
-
-with urllib.request.urlopen(req) as response:
-    body = response.read().decode('utf-8')
     print(body)
