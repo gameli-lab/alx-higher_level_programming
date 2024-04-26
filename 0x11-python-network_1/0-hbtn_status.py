b277c6cf4263603b2661c476ade4906b6b0d06ec
Benjamin Torfu <btorfu@proton.me>
2024-04-26 14:58:06 +0000

    read

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,11 +2,13 @@
 '''This is a urlopen module'''
 import urllib.request
 
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
-    body = res.read()
-    dec = body.decode('UTF-8')
+if __name__ == '__main__':
+    '''using the urllib'''
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        body = res.read()
+        dec = body.decode('UTF-8')
 
-    print('Body response:')
-    print('\t - type: {}'.format(type(body)))
-    print('\t - content: {}'.format(body))
-    print('\t - utf8 content: {}'.format(dec))
+        print('Body response:')
+        print('\t - type: {}'.format(type(body)))
+        print('\t - content: {}'.format(body))
+        print('\t - utf8 content: {}'.format(dec))
