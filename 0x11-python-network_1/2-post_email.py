13d4d7acd384ab2948095d36d635533df725afcf
Benjamin Torfu <btorfu@proton.me>
2024-04-30 12:40:58 +0000

    post

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 '''header module'''
-import urllib.request
+
+import urllib.request, urllib.parse
 import sys
 if __name__ == '__main__':
     url = sys.argv[1]
     email = sys.argv[2]
-    coded = email.encode('UTF-8')
+    email = urllib.parse.urlencode(email)
+    email = email.encode('ascii')
 
-    req = urllib.request.Request(url, data=coded, method='POST')
+    req = urllib.request.Request(url, email)
     with urllib.request.urlopen(req) as res:
         body = res.read().decode('UTF-8')
 
