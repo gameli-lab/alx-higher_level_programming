6e50d6ba83df34a0fdf607a80cb3213812b4f365
Benjamin Torfu <btorfu@proton.me>
2024-05-22 08:07:59 +0000

    js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -5,9 +5,9 @@ const request = require('request');
 const url = process.argv[2];
 
 request.get(url, (error, response) => {
-	if (error) {
-		console.error('Error:', error);
-	} else {
-		console.log('Code:', response.statusCode);
-	}
+  if (error) {
+    console.error('Error:', error);
+  } else {
+    console.log('Code:', response.statusCode);
+  }
 });
--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -7,10 +7,10 @@ const id = process.argv[2];
 const url = 'https://swapi-api.alx-tools.com/api/films/' + id;
 
 request.get(url, (error, response, body) => {
-	if (error) {
-		console.error(error);
-	} else {
-		const data = JSON.parse(body);
-		console.log(data.title);
-	}
+  if (error) {
+    console.error(error);
+  } else {
+    const data = JSON.parse(body);
+    console.log(data.title);
+  }
 });
